6f3c5d1f6f44f9586befa5fe597edcae65d15d62
Artem <adesp.rtm@gmail.com>
2016-03-20 18:10:45 +0300

    Add friendly URLs

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   validates :title, presence: true,
                     length: { minimum: 5 }
+  extend FriendlyId
+  friendly_id :slug, use: [:slugged, :finders]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20160320123505) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.string   "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
