35fd7469dd6110244592e30660e2fbe1e0865fe1
Artem <adesp.rtm@gmail.com>
2016-03-20 16:22:41 +0300

    Fix comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,3 @@
-class Article < ActiveRecord::Base
-  has_many :comments
-  validates :title, presence: true,
-                    length: { minimum: 5 }
+class Comment < ActiveRecord::Base
+  belongs_to :article
 end
